20a4abb670547979f85ef10a03beff068f291ee6
Huiren Jiang <hujia@microsoft.com>
2023-05-19 10:16:30 +0800

    Update version

--- src/k8s-extension/setup.py
+++ src/k8s-extension/setup.py
@@ -33,7 +33,7 @@ CLASSIFIERS = [
 # TODO: Add any additional SDK dependencies here
 DEPENDENCIES = []
 
-VERSION = "1.4.0"
+VERSION = "1.4.0+winfield.preview"
 
 with open("README.rst", "r", encoding="utf-8") as f:
     README = f.read()
